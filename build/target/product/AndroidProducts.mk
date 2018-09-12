9963c5d9e218fad79b8fb0d1f4b9c1aae8c22780
Prema Chand Alugu <premaca@gmail.com>
2018-09-12 21:00:14 +0530

    vendor: Tipsy Pie commence

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -15,6 +15,6 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/gzosp_arm.mk \
-    $(LOCAL_DIR)/gzosp_arm64.mk \
-    $(LOCAL_DIR)/gzosp_x86.mk
+    $(LOCAL_DIR)/tipsy_arm.mk \
+    $(LOCAL_DIR)/tipsy_arm64.mk \
+    $(LOCAL_DIR)/tipsy_x86.mk
